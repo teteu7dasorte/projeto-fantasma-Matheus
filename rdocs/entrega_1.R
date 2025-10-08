3be4b760863bee41fe86e25d5e09c434be7220ee
teteu7dasorte <matheusfcb@gmail.com>
2025-10-08 15:40:51 -0300

    Initial commit

---
+++ rdocs/entrega_1.R
@@ -0,0 +1,52 @@
+source("rdocs/source/packages.R")
+
+# ---------------------------------------------------------------------------- #
+
+#        ______   _____  ________      ________ 
+#      |  ____| / ____| |__   __| /\  |__   __|
+#     | |__    | (___     | |   /  \    | |   
+#    |  __|    \___ \    | |  / /\ \   | |   
+#   | |____   ____) |   | |  /____ \  | |   
+#  |______   |_____/   |_| /_/    \_\|_|   
+#  
+#         Consultoria estatística 
+#
+
+# ---------------------------------------------------------------------------- #
+# ############################## README ###################################### #
+# Consultor, favor utilizar este arquivo .R para realizar TODAS as análises
+# alocadas a você neste projeto pelo gerente responsável, salvo instrução 
+# explícita do gerente para mudança.
+#
+# Escreva seu código da forma mais clara e legível possível, eliminando códigos
+# de teste depreciados, ou ao menos deixando como comentário. Dê preferência
+# as funções dos pacotes contidos no Tidyverse para realizar suas análises.
+# ---------------------------------------------------------------------------- #
+
+##Análise 1
+
+##código pra limpar o banco
+
+library(tidyverse)
+
+
+
+g1 <- ggplot(mpg) +
+  aes(x = class) +
+  geom_bar(fill = "#A11D21") +
+  labs(x = "Classe do automóvel", y = "Frequência") +
+  theme_estat()
+
+print_quadro_resumo(variavel)
+
+
+
+
+
+
+
+
+
+
+
+
